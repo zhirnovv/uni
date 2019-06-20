2dad8076f9552d9f28bdc4a7db48ecd8f242d065
Vasily Zhirnov <vass.zhirnov@gmail.com>
2019-06-20 09:28:15 +0300

    Fix 7-virtuals.cpp

--- C++/2018-19/Exams/S2/7-virtuals.cpp
+++ C++/2018-19/Exams/S2/7-virtuals.cpp
@@ -1,24 +1,29 @@
-// Virtual functions are member functions that are defined in the base class and overridden in the
-// derived class. The function call is resolved at runtime.
+// ---Virtual functions---
+// A virtual function is a member function which is declared in the base class and is overridden by
+// a derived class. 
 //
-// Rules:
-// 1. Not static
-// 2. Accessed via pointer
-// 3. Prototype of function should be the same as prototype of derived function
-// 4. Defined in base class, overriden in derived class.
-// 5. A constructor cannot be virtual.
+// Virtual functions ensure that the correct function is called for an object, regardless of the
+// type of reference (or pointer) used for the function call.
 //
-// If there is a pure virtual function defined, then the class is called an abstract class, or an
-// interface. Interfaces are used to predefine data members and member functions to be reused in
-// different situations.
-
+// Virtual functions are used to achieve runtime polymorphism, also called late function binding
+//
+// The resolving of a function call is done at run-time
+//
+// RULES:
+// 1. Virtual functions cannot be static and cannot be a friend function.
+// 2. Virtual functions should be accessed by pointer or reference of base class type to achieve
+// runtime polymorphism.
+// 3. They are always defined in base class and overridden in derived class.
+// 4. The prototype of virtual functions should be the same in base as well as derived class.
+// 5. A class may have a virtual destructor but not a virtual constructor.
+//
 
 #include <iostream>
 
 using namespace std;
 
 class Base {
-  public:
+  public: 
     virtual void print() {
       cout << "print from base class" << endl;
     }
@@ -27,40 +32,20 @@ class Base {
     }
 };
 
-class Interface {
-  public: 
-    virtual void get() = 0;
-};
-
-class DerivedFromInterface : public Interface {
-  public:
-    void get() {
-      cout << "get from derived class" << endl;
-    }
-};
-
 class Derived : public Base {
-  // override base class
   void print() {
     cout << "print from derived class" << endl;
   }
-
   void show() {
-    cout << "show from base class" << endl;
+    cout << "show from derived class" << endl;
   }
 };
 
 int main() {
   Base *basePointer;
-  Interface *interface;
   Derived derived;
-  DerivedFromInterface derivedFromInterface;
+  basePointer = &derived; // reassign pointer from base class to derived class, this will cause the virtual function to be overridden.
 
-  basePointer = &derived;
-  interface = &derivedFromInterface;
-
-  basePointer -> print(); // virtual function, defined at runtime
-  basePointer -> show(); // normal function, defined at compile time
-
-  interface -> get(); // interface function, defined at runtime and empty at compile time
+  basePointer->print();
+  basePointer->show();
 }
