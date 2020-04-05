648a285bb915ceafd69d95847554d673b4efbf61
Vasily Zhirnov <vass.zhirnov@gmail.com>
2020-04-05 17:21:19 +0300

    Add Queue with minimum immplementation

---
+++ min_queue/main.cpp
@@ -0,0 +1,107 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+
+using namespace std;
+
+struct Queue {
+    int * queue;
+    int queueSize;
+    int head;
+    int tail;
+    int minimum;
+
+    Queue(int _size = 2) {
+        queueSize = _size;
+        head = 0;
+        tail = 0;
+        minimum = INT_MAX;
+        queue = new int [queueSize];
+    }
+
+    ~Queue() {
+        delete [] queue;
+    }
+
+    int min() {
+      if (head == tail) {
+        cout << "Error: queue is empty." << endl;
+        return INT_MAX;
+      }
+      return minimum;
+    }
+
+    void resize() {
+        int new_size = queueSize * 2;
+
+        int * new_queue;
+        new_queue = new int[new_size];
+
+        for (int i=0; i < queueSize; i++){
+            new_queue[i] = queue[i];
+        }
+
+        delete [] queue;
+        queue = new_queue;
+        queueSize = new_size;
+    }
+
+
+    void push(int element) {
+        // if queue is full, resize
+        if (tail + 1 == queueSize) {
+            resize();
+        }
+
+        // enqueue, then shift tail by 1
+        queue[tail++] = element;
+
+        // update minimum in case a new, smaller, number is introduced into the queue
+        if (element < minimum) {
+          minimum = element;
+        }
+    }
+    
+    void pull() {
+        if (head == tail) {
+            cout << "Error: queue is empty." << endl;
+        } else {
+            head++;
+            // update minimum
+            for (int i = head; i < tail; i++)
+              if (queue[i] < minimum) minimum = queue[i];
+        }
+    }
+};
+
+int main() {
+    Queue q;
+    ifstream inFile("in.txt");
+    ofstream outFile("out.txt");
+
+    string command;
+    
+    if (!inFile.good()) {
+      cout << "Error: failed to read file.";
+      return 0;
+    }
+
+    while(inFile >> command) {
+      cout << "new command";
+      switch (command[0]) {
+        case '+':
+          q.push(stoi(command.substr(1)));
+          break;
+        case '-':
+          q.pull();
+          break;
+        case '?':
+          outFile << q.min() << ' ';
+          break;
+        default:
+          cout << "Error: invalid command";
+      } 
+    }
+
+    return 0;
+}
